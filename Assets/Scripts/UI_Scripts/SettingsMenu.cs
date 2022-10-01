7d34aa58c27148f7741aad2d28edc0702768336d
Aurora <enerhaugen12@gmail.com>
2022-10-01 16:11:08 +0200

    Menu UI

---
+++ Assets/Scripts/UI_Scripts/SettingsMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public Dropdown ResolutionDropdown;
+
+    private Resolution[] resolutions;
+    
+    void start()
+    {
+        resolutions = Screen.resolutions;
+        
+        ResolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+        }
+
+        ResolutionDropdown.AddOptions(options);
+    }
+    
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
