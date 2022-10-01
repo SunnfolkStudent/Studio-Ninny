da8eb9dfe79057235d13eb6d87f3f854704acf77
Aurora <enerhaugen12@gmail.com>
2022-10-01 16:32:57 +0200

    menu fix

--- Assets/Scripts/UI_Scripts/SettingsMenu.cs
+++ Assets/Scripts/UI_Scripts/SettingsMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
@@ -20,13 +21,27 @@ public class SettingsMenu : MonoBehaviour
 
         List<string> options = new List<string>();
 
+        int currenResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currenResolutionIndex = i;
+            }
         }
 
         ResolutionDropdown.AddOptions(options);
+        ResolutionDropdown.value = currenResolutionIndex;
+        ResolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
     
     public void SetVolume (float volume)
