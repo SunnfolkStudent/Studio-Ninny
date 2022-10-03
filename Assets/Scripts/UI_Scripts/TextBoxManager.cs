530fecdb8d6a01d1f81872d9ce9e38984923516c
Aurora <enerhaugen12@gmail.com>
2022-10-03 17:46:56 +0200

    NPC Scripts And Assets

--- Assets/Scripts/UI_Scripts/TextBoxManager.cs
+++ Assets/Scripts/UI_Scripts/TextBoxManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,44 +10,58 @@ public class TextBoxManager : MonoBehaviour
     public GameObject textBox;
 
     public TMP_Text theText;
-    
+
     public TextAsset textFile;
     public string[] textLines;
 
     public int currentLine;
     public int endAtLine;
 
-    public PlayerInput playerI;
-    public PlayerMovement playerM;
+    public PlayerInput player;
+
+    public bool isActive;
     
     void Start()
     {
-        playerI = FindObjectOfType<PlayerInput>();
-        playerM = FindObjectOfType<PlayerMovement>();
-
+        player = FindObjectOfType<PlayerInput>();
+        
         if (textFile != null)
-        {
-            textLines = (textFile.text.Split('\n'));
-        }
+        { textLines = (textFile.text.Split('\n')); }
 
         if (endAtLine == 0)
-        {
-            endAtLine = textLines.Length - 1;
-        }
+        { endAtLine = textLines.Length - 1; }
+
+        if (isActive)
+        { EnableTextBox(); }
+        else
+        { DisableTextBox(); }
     }
 
     void Update()
     {
+        if (!isActive)
+        { return; }
         theText.text = textLines[currentLine];
 
-        if (Input.GetKeyDown((KeyCode.Return)))
-        {
-            currentLine += 1;
-        }
+        if (Input.GetKeyDown((KeyCode.F)))
+        { currentLine += 1; }
 
         if (currentLine > endAtLine)
-        {
-            textBox.SetActive(false);
-        }
+        { DisableTextBox(); }
+    }
+
+    public void EnableTextBox()
+    { textBox.SetActive(true);
+        isActive = true; }
+
+    public void DisableTextBox()
+    { textBox.SetActive(false);
+        isActive = false; }
+
+    public void ReloadScript(TextAsset theText)
+    {
+        if (theText != null)
+        { textLines = new String[1];
+            textLines = (theText.text.Split('\n')); }
     }
 }
