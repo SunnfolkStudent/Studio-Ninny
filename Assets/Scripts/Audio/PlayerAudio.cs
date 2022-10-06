1c340a0dd30e58912fe9b6b443c33ab3665357d5
Ilvapstrom <ilvapstrom@gmail.com>
2022-10-06 18:23:55 +0200

    Audio script og shadow tiles

---
+++ Assets/Scripts/Audio/PlayerAudio.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioClip[] attackAudio;
+    public AudioClip[] jumpAudio;
+    public AudioClip[] landingAudio;
+    public AudioClip[] damageAudio;
+    public AudioClip[] curiousAudio;
+
+    private AudioSource _audio;
+
+    private void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    //play in animation
+    public void AttackAudio()
+    {
+        AudioClipRandom(attackAudio);
+    }
+    
+    public void JumpAudio()
+    {
+        AudioClipRandom(jumpAudio);
+    }
+    
+    public void LandingAudio()
+    {
+        AudioClipRandom(landingAudio);
+    }
+    
+    public void DamageAudio()
+    {
+        AudioClipRandom(damageAudio);
+    }
+    
+    public void CuriousAudio()
+    {
+        AudioClipRandom(curiousAudio);
+    }
+    
+    //random clip (and pitch)
+    private void AudioClipRandom(AudioClip[] audioClips)
+    {
+        //_audio.pitch = Random.Range(0.75f, 1.25f);
+        _audio.PlayOneShot(audioClips[Random.Range(0, audioClips.Length)]);
+    }
+}
